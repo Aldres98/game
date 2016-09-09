290068999c4060c274197245629e52655c3761d3
Aldres98 <aldres98@gmail.com>
2016-09-09 16:43:06 +0300

    boot.js fix

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,9 +1,11 @@
-var game = new Phaser.Game(800, 512, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update });
+//= require phaser
+
+var game = new Phaser.Game(800, 512, Phaser.AUTO, 'gameDiv');
 
 game.state.add('boot', bootState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
 game.state.add('win', winState);
-game.state.add('lose', loseState);
+game.state.add('load', loadState);
 
 game.state.start('boot');
--- app/assets/javascripts/load.js
+++ app/assets/javascripts/load.js
@@ -1,12 +1,21 @@
 var loadState = {
 
-    function preload(){
+     preload: function(){
+
+
+        var loadingLabel = game.add.text(80, 150, 'loading...'{fontSize:'32px', fill:'#ffff'})
 
     game.load.image('star', '/assets/star.png');
     game.load.image('sky', '/assets/sky.png');
     game.load.image('ground', '/assets/ground.png');
     game.load.spritesheet('dude', '/assets/dude.png', 32, 48);
-    game.load.spritesheet('spike', '/assets/spike/')
+    game.load.spritesheet('spike', '/assets/spike/');
+
+    }
+
+    create: function(){
+
+        game.state.start('menu';)
 
     }
 };
