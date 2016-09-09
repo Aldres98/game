c522925bef5c894181f2e6c18f318f48c060133a
Aldres98 <aldres98@gmail.com>
2016-09-09 09:49:37 +0300

    Boot.js and load.js for code refactoring

---
+++ app/assets/javascripts/load.js
@@ -0,0 +1,12 @@
+var loadState = {
+
+    function preload(){
+
+    game.load.image('star', '/assets/star.png');
+    game.load.image('sky', '/assets/sky.png');
+    game.load.image('ground', '/assets/ground.png');
+    game.load.spritesheet('dude', '/assets/dude.png', 32, 48);
+    game.load.spritesheet('spike', '/assets/spike/')
+
+    }
+};
